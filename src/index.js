0ad0791745c3f3c56dbe41a70ad145384cc5a1ee
Stefano Dallona <stefano.dallona@electrolux.com>
2023-04-28 18:08:20 +0200

    Refactoring to merge plc-testbench changes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App';
+import * as serviceWorker from './serviceWorker';
+
 import reportWebVitals from './reportWebVitals';
 import { RunHistory } from './pages/RunHistory';
 import { RunConfiguration } from './pages/RunConfiguration';
@@ -32,3 +34,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
